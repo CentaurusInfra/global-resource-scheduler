fc4b2fae880c7e99ae2ff8dc7e93a9f973cd0b69
Wang Jun <guaklw@163.com>
2020-11-28 17:02:10 +0800

    remove podgc for gs-scheduler

--- cmd/kube-controller-manager/app/controllermanager.go
+++ cmd/kube-controller-manager/app/controllermanager.go
@@ -381,7 +381,9 @@ const (
 func NewControllerInitializers(loopMode ControllerLoopMode) map[string]InitFunc {
 	controllers := map[string]InitFunc{}
 	controllers["endpoint"] = startEndpointController
-	controllers["podgc"] = startPodGCController
+	// NOTE(nkwangjun): we should disable podgc for gs-scheduler, if we binding a cluster to the pod,
+	// it will be deleted by the podgc.
+	//controllers["podgc"] = startPodGCController
 	controllers["vmpod"] = startVMPodController
 	controllers["namespace"] = startNamespaceController
 	controllers["tenant"] = startTenantController
