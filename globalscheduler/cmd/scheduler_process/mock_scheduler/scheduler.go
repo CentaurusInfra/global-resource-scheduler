d36ed60e1d13a254089505b4ead6f63036a56a61
Jun Shao <jshao@futurewei.com>
2021-01-12 03:04:48 +0000

    Updated to PodAssigned

--- globalscheduler/cmd/scheduler_process/mock_scheduler/scheduler.go
+++ globalscheduler/cmd/scheduler_process/mock_scheduler/scheduler.go
@@ -55,16 +55,17 @@ func NewScheduler(config *rest.Config, podQueue chan *v1.Pod, name string, quit 
 		if err != nil {
 			klog.Fatal(err)
 		}
-		clusterMap[name] = scheduler.Spec.Cluster
+		clusterMap[name] =  scheduler.Spec.Cluster
 	} else {
 		schedulerList, err := schedulerclientset.GlobalschedulerV1().Schedulers("default").List(metav1.ListOptions{})
 		if err != nil {
 			klog.Fatal(err)
 		}
-		for _, scheduler := range schedulerList.Items {
-			clusterMap[scheduler.Name] = scheduler.Spec.Cluster
+		for _,scheduler := range schedulerList.Items {
+			clusterMap[scheduler.Name] =  scheduler.Spec.Cluster
 		}
 	}
+
 
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
@@ -106,9 +107,9 @@ func (s *Scheduler) initInformers(quit chan struct{}) {
 		},
 	})
 
-	podSelector := fields.ParseSelectorOrDie("status.phase=assigned,status.assignedScheduler.name!=''")
+	podSelector := fields.ParseSelectorOrDie("status.phase="+string(v1.PodAssigned)+",status.assignedScheduler.name!=''")
 	if len(s.name) > 0 {
-		podSelector = fields.ParseSelectorOrDie("status.phase=assigned,status.assignedScheduler.name=" + s.name)
+		podSelector = fields.ParseSelectorOrDie("status.phase="+string(v1.PodAssigned)+",status.assignedScheduler.name=" + s.name)
 	}
 
 	lw := cache.NewListWatchFromClient(s.clientset.CoreV1(), string(v1.ResourcePods), metav1.NamespaceAll, podSelector)
@@ -139,7 +140,7 @@ func (s *Scheduler) initInformers(quit chan struct{}) {
 				return
 			}
 			fmt.Printf("A  pod %s has been updated\n", newPod.Name)
-			if oldPod.Status.Phase != "assigned" && newPod.Status.Phase == "assigned" {
+			if oldPod.Status.Phase != v1.PodAssigned && newPod.Status.Phase == v1.PodAssigned {
 				s.podQueue <- newPod
 			}
 		},
