1151a1de03434eaede6bccf2a4890d688f9fddf7
kimeunju108 <ekim@futurewei.com>
2021-01-26 00:28:52 +0000

    update one pod yaml file

---
+++ globalscheduler/test/data_createdelete_tool/delete_pod.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Copyright 2020 Authors of Arktos - file created.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#Example: ./delete_cluster 10 20 => delete cluster10 ~ cluster20
+counter=$1
+while [ $counter -le $2 ]
+do
+    name="kubectl delete pod pod-$counter"
+    echo $name
+    $name
+     ((counter++))
+done
